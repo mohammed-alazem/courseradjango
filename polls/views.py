f2306a9e37b3d9bd5ef9910c6feb03a72d8c174f
ASUS <mohammedalazem11@gmail.com>
2022-09-17 19:08:14 +0300

    2

--- polls/views.py
+++ polls/views.py
@@ -8,8 +8,8 @@ from polls.models import Question
 # Create your views here.
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    # return render(request, 'polls/detail.html', {'question': question})
-    return HttpResponse("Answer to the Ultimate Question.")
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 
 def results(request, question_id):
