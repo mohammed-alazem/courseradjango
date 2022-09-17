75a19126158895065933f09d69db4586e5a10a88
ASUS <mohammedalazem11@gmail.com>
2022-09-17 18:22:47 +0300

    2

--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,8 @@ from polls.models import Question
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
@@ -18,9 +20,11 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 
-
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+
+def owner(request):
+    return HttpResponse("Hello, world. c414093e is the polls index.")
